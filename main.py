7565db1b5fe0d1b90be6a4cde4c1b45b892a913b
user <biancavell@hotmail.com>
2023-04-14 09:31:28 -0300

    feat: completed Tafera 3 successfully > passing all testsgit add .!

---
+++ main.py
@@ -0,0 +1,12 @@
+from teams.utils import data_processing
+
+
+data_processing(
+    {
+        "name": "França",
+        "titles": 9,
+        "top_scorer": "Zidane",
+        "fifa_code": "FRA",
+        "first_cup": "2002-10-18",
+    }
+)
